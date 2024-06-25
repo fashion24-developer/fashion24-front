079b8e10ec71f59ed297f9b4f0bfba88f45a66bf
zzzRYT <jinjinstar3@gmail.com>
2024-06-25 15:57:39 +0900

    setting(#11):styled-components추가 theme setup

--- src/components/MockTestComponent.tsx
+++ src/components/MockTestComponent.tsx
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 import { useEffect } from 'react';
+import styled from 'styled-components';
 
 const MockTestComponent = () => {
   const testFunction = async () => {
@@ -17,9 +18,16 @@ const MockTestComponent = () => {
 
   return (
     <div>
+      <TestStyled />
       <div>모킹 데이터 테스트</div>
     </div>
   );
 };
+
+const TestStyled = styled.div`
+  width: 100px;
+  height: 100px;
+  color: #f00;
+`;
 
 export default MockTestComponent;

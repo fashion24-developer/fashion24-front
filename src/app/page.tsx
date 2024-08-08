84f7c528f1d76d7b86dae49b405034aadcbf9427
zzzRYT <jinjinstar3@gmail.com>
2024-08-08 21:51:14 +0900

    refactor(#40): style-component dependencise제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { ThemeProvider } from 'styled-components';
 import Root from './root/page';
 import { themeColor } from '@/styles/theme.css';
 
 export default function Home() {
   return (
-    <ThemeProvider theme={themeColor}>
+    <>
       <Root />
-    </ThemeProvider>
+    </>
   );
 }

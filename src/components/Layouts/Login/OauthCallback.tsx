79efb2c012598c91ecd2aa8ec497c5f388b3cc8a
zzzRYT <jinjinstar3@gmail.com>
2024-08-20 14:41:01 +0900

    feat(#53): login후 루트 페이지로 이동

--- src/components/Layouts/Login/OauthCallback.tsx
+++ src/components/Layouts/Login/OauthCallback.tsx
@@ -1,20 +1,29 @@
 'use client';
 
 import AUTH from '@/apis/auth';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 const OAuthCallbackPage = ({ provider }: { provider: string }) => {
+  const [isToken, setIsToken] = useState(false);
+  const router = useRouter();
   const params = useSearchParams();
   const code = params.get('code');
 
   const authHandler = async () => {
     await AUTH.authLogin(code as string, provider);
+    setIsToken(true);
   };
 
   useEffect(() => {
     authHandler();
   }, []);
+
+  useEffect(() => {
+    if (isToken) {
+      router.push('/');
+    }
+  });
 
   return <div>Loading</div>;
 };

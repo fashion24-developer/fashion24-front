b8fa012114bc4c65d0371814d0f56df86ab4b1b0
zzzRYT <jinjinstar3@gmail.com>
2024-08-24 16:08:10 +0900

    setting(#46): three.js R3F setup

---
+++ src/app/whatever/page.tsx
@@ -0,0 +1,14 @@
+import WhateverHeader from '@/components/Layouts/Whatever/WhateverHeader';
+import WhateverTestComp from '@/components/Layouts/Whatever/WhateverTestComp';
+import Header from '@/components/UI/Header/Header';
+
+const WhateverPage = () => {
+  return (
+    <div>
+      <WhateverHeader />
+      <WhateverTestComp />
+    </div>
+  );
+};
+
+export default WhateverPage;

cde901390093d7724b3f82a6315423116b0e5fd7
zzzRYT <jinjinstar3@gmail.com>
2024-08-29 18:17:26 +0900

    feat(#48): query에 값을 추가하는 hook 구현

--- src/components/Layouts/Fancy/unit/PrintColorOptions.tsx
+++ src/components/Layouts/Fancy/unit/PrintColorOptions.tsx
@@ -5,49 +5,38 @@ import * as style from '@/components/Layouts/Fancy/fancy.css';
 import { assignInlineVars } from '@vanilla-extract/dynamic';
 import { OptionsType } from '@/types/fancy';
 import { useEffect, useState } from 'react';
+import useSearchQuery from '@/hooks/useSearchQuery';
 
 interface PrintColorOptionsProps {
   selectedColors: OptionsType['subOptions'];
 }
 
+interface SearchQueryType {
+  selectColor: string;
+  name: string;
+}
+
 const PrintColorOptions = ({ selectedColors }: PrintColorOptionsProps) => {
-  const [searchQuery, setSearchQuery] = useState({
+  const initialQuery: SearchQueryType = {
     selectColor: '',
+    name: '',
+  };
+  const { searchQuery, queryHandler } = useSearchQuery<SearchQueryType>({
+    query: initialQuery,
   });
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const router = useRouter();
-
-  const colorHandler = (color: string) => {
-    const updatedQuery = { ...searchQuery, selectColor: color };
-    setSearchQuery(updatedQuery);
-    updateSearchQuery(updatedQuery);
-  };
-
-  const updateSearchQuery = (updatedQuery: any) => {
-    const params = new URLSearchParams(searchParams);
-    Object.keys(updatedQuery).forEach(key => {
-      if (updatedQuery[key]) {
-        params.set(key, updatedQuery[key]);
-      } else {
-        params.delete(key);
-      }
-    });
-    const queryString = params.toString();
-    const updatedPath = queryString ? `${pathname}?${queryString}` : pathname;
-    router.push(updatedPath);
-  };
   if (selectedColors) {
     return (
       <>
         {selectedColors.map(color => {
           return (
-            <div
-              onClick={() => colorHandler(color.name)}
-              key={color.id}
-              style={assignInlineVars({ [style.colorOptionVar]: color.name })}
-              className={style.colorOption}
-            ></div>
+            <div>
+              <div
+                onClick={() => queryHandler('selectColor', color.name)}
+                key={color.id}
+                style={assignInlineVars({ [style.colorOptionVar]: color.name })}
+                className={style.colorOption}
+              ></div>
+            </div>
           );
         })}
       </>

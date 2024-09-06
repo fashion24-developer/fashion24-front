d5a6520323538f344926227d9fe14fecbd5735fb
zzzRYT <jinjinstar3@gmail.com>
2024-09-06 15:10:22 +0900

    publish(#70):jotai atom추가

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -2,9 +2,11 @@
 
 import { useState } from 'react';
 import * as styles from './checkBox.css';
+import { useAtom } from 'jotai';
+import { checkBoxAtom } from '@/jotai/atoms/uiAtom';
 
 export default function CheckBox() {
-  const [check, setCheck] = useState(false);
+  const [check, setCheck] = useAtom(checkBoxAtom);
 
   return (
     <div className={styles.checkBoxContainer}>

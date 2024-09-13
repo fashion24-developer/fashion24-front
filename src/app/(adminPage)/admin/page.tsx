e5c45a4cff4ac0c28098ede5b842cbdefdacde32
zzzRYT <jinjinstar3@gmail.com>
2024-09-13 17:58:57 +0900

    modify(#84):admin page route추가

--- src/app/(adminPage)/admin/page.tsx
+++ src/app/(adminPage)/admin/page.tsx
@@ -1,6 +1,10 @@
-import Link from 'next/link';
-import * as styles from './admin.css';
+'use client';
 
+import { useRouter } from 'next/navigation';
+
+//해당 부분은 나중에 미들웨어를 통해 깜빡임 없이 이동
 export default function AdminPage() {
+  const router = useRouter();
+  router.replace('/admin/products');
   return <>어드민 페이지</>;
 }
